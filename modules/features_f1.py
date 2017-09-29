bca4fc968d59d5c253471c6637685467150edded
skryjomskipl <skryjomskipl@vcu.edu>
2017-09-28 23:07:42 -0400

    Cleanup in feature extractor w/ feature retrieved from the POS tagger.

--- modules/features_f1.py
+++ modules/features_f1.py
@@ -19,11 +19,11 @@ class FeaturesF1:
         a = abstract.get_entity_ids(rel.a)
         b = abstract.get_entity_ids(rel.b)
 
-        # An example of a pathetic feature, word distance:
+        # Feature 1 - Word distance between tags
         distance = min(b)-max(a)
         X.append(distance)
 
-        # Prepare data for the POS tagging
+        # Feature 2 - POS tag of the last word in the entity sequence
         tokens = []
 
         for obj in abstract.obj:
@@ -31,11 +31,9 @@ class FeaturesF1:
         
         tokens_pos = nltk.pos_tag(tokens)
 
-        # Using a POS tagger for both entities
-        # Taking only into account last word of the entity sequence
-
         a_id = max(a)
         b_id = max(b)
+        
         a_pos = tokens_pos[a_id][1]
         b_pos = tokens_pos[b_id][1]
 
